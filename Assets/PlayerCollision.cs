76f44de7c45a8bfaeac0e0ec8c98e471f0f3df09
sw1ft-r <163565727+sw1ft-r@users.noreply.github.com>
2024-03-15 20:42:07 +0200

    end of tutorial episode 5 (collisions and obstacles)
    added collisions and obstacles for future stuff xd
    the 'player' now stops when hitting an obstacle, that will probably do a game over (idk maybe will be implemented in future)

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle!");
+            movement.enabled = false;
+        }
+    }
+
+}
