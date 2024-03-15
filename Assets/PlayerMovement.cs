9337b5e3d766373c8b036af4d68291125a824335
sw1ft-r <163565727+sw1ft-r@users.noreply.github.com>
2024-03-15 20:16:50 +0200

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    //referencing Rigidbody as rb
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // FIXEDUPDATE CAUSE PHYSICS RAHHH
+    void FixedUpdate()
+    {
+        // Add forward force
+        rb.AddForce (0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if ( Input.GetKey("a") )
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
