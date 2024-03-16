95b0839a87a0e59526979cf6f91015310c3a57ac
sw1ft-r <163565727+sw1ft-r@users.noreply.github.com>
2024-03-16 19:04:49 +0200

    episode 8 - game over

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -9,7 +9,8 @@ public class PlayerCollision : MonoBehaviour
         if (collisionInfo.collider.tag == "Obstacle")
         {
             Debug.Log("We hit an obstacle!");
-            movement.enabled = false;
+            GetComponent<PlayerMovement>().enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 
